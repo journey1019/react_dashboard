d04d86341c9dbad99726642b998df3426165bfdf
ijihyeon <jhlee@orbcomm.co.kr>
2023-05-11 13:49:35 +0900

    test(Components_Navbar): Add Mui_AppNavbar & Remove Error

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -179,4 +179,5 @@ const Navbar = () => {
     );
 };
 
-export default Navbar;*/
+export default Navbar;
+*/
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
 import BasicNavbar from "../../components/navbar/BasicNavbar";
 import "./home.scss";
 import TextField from '@mui/material/TextField';
-
+import Header from "../../components/navbar/Header";
 
 const Home = () => {
   return (
       <>
       <div className="home">
-        <BasicNavbar />
-        <div className="homeContainer">
+          <BasicNavbar />
+          <div className="homeContainer">
           <h1>NMS Main Home Page</h1>
             <div className="login">
                 <TextField id="outlined-basic" label="Outlined" variant="outlined"  />

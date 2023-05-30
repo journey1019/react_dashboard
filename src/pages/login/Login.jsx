6e859e4cb298b6136c63086415ff11a5a72e7824
ijihyeon <jhlee@orbcomm.co.kr>
2023-05-30 15:51:48 +0900

    코드수정(Login): MUI Login Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,43 +1,83 @@
-import "./login.scss"
-import profile from './imgs/profile.png';
-import email from './imgs/email.jpg';
-import pass from './imgs/pass.png';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function Login() {
+export default function SignIn() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
 
-    return(
-        <>
-            <div className="main">
-                <div className="sub-main">
-                    <div>
-                        <div className="imgs">
-                            <div className="container-image">
-                                <img src={profile} alt="profile" className="profile"/>
-                            </div>
-                        </div>
-                        <div>
-                            <h1>Login Page</h1>
-                            <div>
-                                <img src={email} alt="email" className="email"/>
-                                <input type="text" placeholder="user name" className="name"/>
-                            </div>
-                            <div className="second-input">
-                                <img src={pass} alt="pass" className="email"/>
-                                <input type="password" placeholder="user name" className="name"/>
-                            </div>
-                            <div className="login-button">
-                                <button>Login</button>
-                            </div>
-
-                            <p className="link">
-                                <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
     );
 }
-
-export default Login;

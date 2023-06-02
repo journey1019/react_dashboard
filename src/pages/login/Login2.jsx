d3ecd68d07a2a77e2adfc02b1e0958a089262967
ijihyeon <jhlee@orbcomm.co.kr>
2023-06-02 10:05:31 +0900

    코드수정(Login): 로그인 페이지 바꿈(Login<->Login2)

--- src/pages/login/Login2.jsx
+++ src/pages/login/Login2.jsx
@@ -1,3 +1,5 @@
+// Login Error
+/* mui */
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -7,150 +9,140 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom'
 
+import React, { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "./context/AuthProvider";
 
-import Logo from "../../assets/KO_logo.png";
-import Session from 'react-session-api';
-import axios from 'axios';
+import axios from './api/axios';
+
+
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const { setAuth } = useContext(AuthContext);
+
+    // user, error DOM
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
 
     const navigate = useNavigate();
 
-
     useEffect(() => {
-        const auth = localStorage.getItem('user');
-        if(auth) {
-            navigate("/")
-        }
+        userRef.current.focus();
     }, [])
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({ // 입력한 데이터 출력
-            username: data.get("username"),
-            password: data.get("password"),
-        });
-    };
+    useEffect(()=>{
+        setErrMsg('');
+    }, [user, pwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        /*console.log({
+            user: data.get("user"),
+            pwd: data.get("pwd"),
+            }
+        )*/
+        const LOGIN_URL = 'https://iotgwy.commtrace.com/restApi/user/login';
+        const PARAMS = {userId: user, userPw: pwd}
 
-    async function signIn() {
-        let item = {username, password};
-        console.warn(item);
-
-        const urls = "https://iotgwy.commtrace.com/restApi/user/login";
-        const params = {userId: username, userPw: password}
-        const headers = {
-            "Accept": "application/json",
-        }
-
-        let returnVal = null;
-
+        // 비동기가 있는 가중치 알림으로 api dir 내부에 있는 axios file에 정의한
+        // 기본 URL에 추가된 로그인 URL을 전달하고
+        // axios의 두 번째 매개변수는 json.stringify가 될 것임
         try {
-            let result = await axios({
-                method : "POST",
-                url: urls,
-                header: headers,
-                params: params,
-                responseType: "json"
-            })
-                .then(response => {
-                    //성공 시, returnVal로 데이터 input
-                    returnVal = response.data.response;
-                    localStorage.setItem("user-info", JSON.stringify(returnVal));
-                    navigate("/login/seLogin")
-                    navigator.push("/seLogin")
-                    //alert("카카오워크로 전송된 2차 인증")
-                    console.log(returnVal);
-                })
-                .then(err => {
-                    return null;
-                });
-            return returnVal;
-        }
-        catch {
-            return null;
+            const response = await axios.post(LOGIN_URL,
+                //JSON.stringify({PARAMS}),
+                {
+                    params : {PARAMS}
+                },
+                {
+                    headers: {
+                        //"Content-Type": `application/json;charset=UTF-8`,
+                        //'Accept': 'application/json' ,
+                        //'Access-Control-Allow-Origin' : '*',
+                        //'Access-Control-Allow-Origin' : 'https://iotgwy.commtrace.com',
+                    },
+                    //withCredentials: true
+                },
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            /*const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;*/
+            // 기록 + 덧붙이기
+            setAuth({ user, pwd });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) { // 오류수신
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            }
+            else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            }
+            else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            }
+            else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
         }
     }
 
     return(
-        <>
-            <div className="logo">
-                {/*<img src={Logo} alt="logo" height="70" width="230" />*/}
-            </div>
-            <Container component="main" maxWidth="xs">
-                <Box
-                    sx={{
-                        marginTop: 10,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
+        <div className="body">
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="/dashboard">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
                             id="username"
-                            label="User Name"
-                            name="username"
-                            value={username}
-                            autoComplete="username"
-                            autoFocus
-                            onChange={e => setUsername(e.target.value)}
-                        />
-                        <TextField
-                            margin="normal"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
                             required
-                            fullWidth
-                            name="password"
-                            value={password}
-                            label="Password"
+                        />
+
+                        <label htmlFor="password">Password:</label>
+                        <input
                             type="password"
                             id="password"
-                            autoComplete="current-password"
-                            onChange={e => setPassword(e.target.value)}
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
                         />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={signIn}
-                        >
-                            Login
-                        </Button>
-
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </>
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        Need an Account?<br />
+                        <span className="line">
+                                {/*put router link here*/}
+                            <a href="/register">Sign Up</a>
+                            </span>
+                    </p>
+                </section>
+            )}
+        </div>
     )
 }
 
-export default Login;
+export default Login

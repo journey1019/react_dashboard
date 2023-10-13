4c9f2b7c8df32c9791aefbdc8e16095a1e29f801
ijihyeon <jhlee@orbcomm.co.kr>
2023-10-13 16:11:24 +0900

    FIX : Diagnostic _ IoParam Tab의 Table 수정

--- src/components/diagnostic/params/ioParam/IoParam.jsx
+++ src/components/diagnostic/params/ioParam/IoParam.jsx
@@ -244,7 +244,7 @@ const IoParam = (props) => {
                                         </TabPanel>
 
                                         {/* Table */}
-                                        <TabPanel value={value} index={0} dir={theme.direction}>
+                                        <TabPanel value={value} index={1} dir={theme.direction}>
                                             <MaterialReactTable
                                                 title="IoParam"
                                                 columns={ioParamColumns}
